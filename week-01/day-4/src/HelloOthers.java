0d0412fc5d6b17e45116778fa2af6f1408f43950
ViolaZsombor <vzsmoney@gmail.com>
2019-09-12 12:59:47 +0200

    helloothers and HumptyDumpty projects added

---
+++ week-01/day-4/src/HelloOthers.java
@@ -0,0 +1,7 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello Omar");
+        System.out.println("Hello Dia");
+        System.out.println("Hello Laci");
+    }
+}
