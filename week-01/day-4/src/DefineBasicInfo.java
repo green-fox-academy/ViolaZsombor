2292cfd533608f4caa75438f1e152143c91b5651
ViolaZsombor <vzsmoney@gmail.com>
2019-09-12 16:51:59 +0200

    added edits to defineBasicInfo and Swap and added VariableMutataion projects

--- week-01/day-4/src/DefineBasicInfo.java
+++ week-01/day-4/src/DefineBasicInfo.java
@@ -1,2 +1,15 @@
+import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
+
 public class DefineBasicInfo {
+    public static void main(String[] args) {
+        String name = "Viola Zsombor";
+        System.out.println(name);
+        int age = 30;
+        System.out.println( age);
+        double height = 1.74;
+        System.out.println(height);
+        boolean married = false;
+        System.out.println(married);
+
+    }
 }
--- week-01/day-4/src/Swap.java
+++ week-01/day-4/src/Swap.java
@@ -1,5 +1,23 @@
 public class Swap {
     public static void main(String[] args) {
+        int a = 123;
+        int b = 526;
+        int seged  = a;
+        a = b;
+        b = seged;
+        System.out.println(a);
+        System.out.println(b);
+
+       /*segedvaltozo nelkul:
+        int a = 3;
+        int b = 4;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println(a);
+        System.out.println(b);*/
+
 
     }
 
