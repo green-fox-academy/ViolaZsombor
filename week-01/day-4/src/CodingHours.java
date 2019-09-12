bba32dd75c135eca15fca631df7d9532b88414e7
ViolaZsombor <vzsmoney@gmail.com>
2019-09-12 14:21:49 +0200

    IntroduceYourself and TwoNumbers projects added

---
+++ week-01/day-4/src/CodingHours.java
@@ -0,0 +1,2 @@
+public class CodingHours {
+}
---
+++ week-01/day-4/src/IntroduceYourself.java
@@ -0,0 +1,7 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        System.out.println("Viola Zsombor");
+        System.out.println(30);
+        System.out.println(1.74);
+    }
+}
