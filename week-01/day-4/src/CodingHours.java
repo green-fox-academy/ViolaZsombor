703ce49957f6f530718f115ac946f887913b1ccf
ViolaZsombor <vzsmoney@gmail.com>
2019-09-12 15:19:27 +0200

    bmi, DefineBasicInfo, FavoriteNumber, Swap projects added

--- week-01/day-4/src/CodingHours.java
+++ week-01/day-4/src/CodingHours.java
@@ -1,2 +1,12 @@
 public class CodingHours {
+    public static void main(String[] args) {
+        int avgCodingHours = 6;
+        int semesterPeriodInWeeks = 17;
+        int workingDays = 5;
+        int weeklyWorkHours = 52;
+        System.out.println(avgCodingHours * workingDays * semesterPeriodInWeeks );
+        System.out.println(((((float)avgCodingHours * workingDays) / weeklyWorkHours ) * 100) + " %");
+        //                      (6   *    5) / 52 = 0,57
+
+    }
 }
---
+++ week-01/day-4/src/FavoriteNumber.java
@@ -0,0 +1,6 @@
+public class FavoriteNumber {
+    public static void main(String[] args) {
+        int favnumb = 3;
+        System.out.println("My favorite number is: " + favnumb);
+}
+}
