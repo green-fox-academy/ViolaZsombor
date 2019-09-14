f0b9424ef540d52454be1bb492097e87b4b7a5e9
ViolaZsombor <vzsmoney@gmail.com>
2019-09-14 15:49:47 +0200

    proba

---
+++ gyakorlas/src/proba.java
@@ -0,0 +1,9 @@
+public class proba {
+    public static void main(String[] args) {
+        int a = 0;
+        while (a < 500) {
+            System.out.println(a);
+            a +=2;
+        }
+    }
+}
