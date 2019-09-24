f381de28dfdb2bfdec1cb255926084035fd1df0a
ViolaZsombor <vzsmoney@gmail.com>
2019-09-24 15:56:17 +0200

    Added WriteMultipleine project

---
+++ week-03/day-1/src/CopyFile.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CopyFile {
+
+  public static void main(String[] args) {
+    
+
+  }
+}
