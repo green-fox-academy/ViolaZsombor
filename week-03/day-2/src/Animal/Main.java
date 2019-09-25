6bd8ffbbc4810b1ad1dcb61d6eb4174dfd72702f
ViolaZsombor <vzsmoney@gmail.com>
2019-09-25 14:28:29 +0200

    Added Animal project

---
+++ week-03/day-2/src/Animal/Main.java
@@ -0,0 +1,13 @@
+package Animal;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Animal lion = new Animal();
+
+    lion.drink();
+    lion.eat();
+    lion.play();
+  }
+
+}
