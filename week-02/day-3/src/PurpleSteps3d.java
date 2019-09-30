c0b67f097b24efb06ecbe679c0c3945bfb0df417
ViolaZsombor <vzsmoney@gmail.com>
2019-09-30 12:37:13 +0200

    mondified Purples3ds project from week2

--- week-02/day-3/src/PurpleSteps3d.java
+++ week-02/day-3/src/PurpleSteps3d.java
@@ -6,45 +6,44 @@ import java.awt.*;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class PurpleSteps3d {
-    public static void mainDraw(Graphics graphics) {
-        // Reproduce this:
-        // [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/assets/r3.png]
-        int x = 10;
-        int y = 10;
-        int size = 10;
 
-        for (int i = 0; i < 6; i++)
-        {
-            graphics.setColor(Color.magenta);
-            graphics.fillRect(x, y, size, size);
-            graphics.setColor(Color.BLACK);
-            graphics.drawRect(x, y, size, size);
-            x += size;
-            y += size;
-            size += 10;
-        }
+  public static void mainDraw(Graphics graphics) {
+    int x = 10;
+    int y = 10;
+    int size = 10;
+
+    for (int i = 0; i < 6; i++) {
+      graphics.setColor(Color.magenta);
+      graphics.fillRect(x, y, size, size);
+      graphics.setColor(Color.BLACK);
+      graphics.drawRect(x, y, size, size);
+      x += size;
+      y += size;
+      size += 10;
     }
+  }
 
-    // Don't touch the code below
-    static int WIDTH = 320;
-    static int HEIGHT = 320;
+  // Don't touch the code below
+  static int WIDTH = 320;
+  static int HEIGHT = 320;
 
-    public static void main(String[] args) {
-        JFrame jFrame = new JFrame("Drawing");
-        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        ImagePanel panel = new ImagePanel();
-        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        jFrame.add(panel);
-        jFrame.setLocationRelativeTo(null);
-        jFrame.setVisible(true);
-        jFrame.pack();
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    ImagePanel panel = new ImagePanel();
+    panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.add(panel);
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+    jFrame.pack();
+  }
+
+  static class ImagePanel extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
     }
-
-    static class ImagePanel extends JPanel {
-        @Override
-        protected void paintComponent(Graphics graphics) {
-            super.paintComponent(graphics);
-            mainDraw(graphics);
-        }
-    }
+  }
 }
